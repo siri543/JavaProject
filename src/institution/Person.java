57efa4208ea304f48b0336d3b39d4a2c236f20cf
siri543 <siris6089@gmail.com>
2023-06-07 08:59:13 -0400

    modified

--- src/institution/Person.java
+++ src/institution/Person.java
@@ -7,6 +7,20 @@ public class Person {
 	String Pphone="5597129007";
 	float Pheight;
 	float Pweight;
-	
+	public void displayDetails()
+	{
+		System.out.println(Pname +" "+ Page);
+	}
+	public String showPemail()
+	{
+		return Pemail;
+		
+	}
+	public static void main(String args[]) {
+		Person p=new Person();
+		p.displayDetails();
+		System.out.println(p.showPemail());
+		
+	}
 
 }
--- src/institution/Student.java
+++ src/institution/Student.java
@@ -1,11 +1,27 @@
 package institution;
 
 public class Student {
-	String name;
-	int ID;
-	String department;
-	float GPA;
-	int phone;
 	
+	public String name;
+	public int ID;
+	public String department;
+	public double GPA;
+	public int phone;
+	public String displayName(String name) {
+		return name;
+		
+		
+	}
+	public void showGPA (double GPA) {
+		System.out.println(GPA);
+		
+	}
+	public static void main(String args[])
+	{
+		Student s=new Student();
+		System.out.println(s.displayName("siri"));
+		s.showGPA(3.5);
+		
+	}
 
 }
